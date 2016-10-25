cf9642491be43b2c30a0336cc2f30e2bc82c616d
Martin Rist <mlr-github@martinrist.com>
2016-10-25 13:43:17 +0100

    Add examples of configuring app

--- src/main/java/com/martinrist/phonebook/App.java
+++ src/main/java/com/martinrist/phonebook/App.java
@@ -2,7 +2,6 @@ package com.martinrist.phonebook;
 
 
 import io.dropwizard.Application;
-import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.slf4j.Logger;
@@ -12,18 +11,23 @@ import org.slf4j.LoggerFactory;
  * Hello world!
  *
  */
-public class App extends Application<Configuration>
+public class App extends Application<PhonebookConfiguration>
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
     @Override
-    public void initialize(Bootstrap<Configuration> bootstrap) {}
+    public void initialize(Bootstrap<PhonebookConfiguration> bootstrap) {}
 
     @Override
-    public void run(Configuration configuration, Environment environment) throws Exception
+    public void run(PhonebookConfiguration configuration, Environment environment) throws Exception
     {
         LOGGER.info("Method App#run() called");
-        System.out.println("Hello world, by Dropwizard");
+
+        for (int i = 0; i < configuration.getMessageRepetitions(); i++) {
+            System.out.println(configuration.getMessage());
+        }
+
+        System.out.println(configuration.getAdditionalMessage());
     }
 
     public static void main(String[] args) throws Exception
---
+++ src/main/java/com/martinrist/phonebook/PhonebookConfiguration.java
@@ -0,0 +1,33 @@
+package com.martinrist.phonebook;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Max;
+
+public class PhonebookConfiguration extends Configuration
+{
+    @JsonProperty
+    @NotEmpty
+    private String message;
+
+    @JsonProperty
+    @Max(10)
+    private int messageRepetitions;
+
+    @JsonProperty
+    private String additionalMessage = "This is optional";
+
+    public int getMessageRepetitions() {
+        return messageRepetitions;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAdditionalMessage() {
+        return additionalMessage;
+    }
+}
