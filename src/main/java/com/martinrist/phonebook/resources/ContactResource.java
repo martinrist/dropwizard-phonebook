d705ead94c4956d7895caf45d7e16658a1aa8fb9
Martin Rist <mlr-github@martinrist.com>
2016-10-25 14:36:20 +0100

    Add in representation for Contact resource

--- src/main/java/com/martinrist/phonebook/resources/ContactResource.java
+++ src/main/java/com/martinrist/phonebook/resources/ContactResource.java
@@ -1,5 +1,7 @@
 package com.martinrist.phonebook.resources;
 
+import com.martinrist.phonebook.representations.Contact;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,11 +14,11 @@ public class ContactResource {
     @Path("/{id}")
     public Response getContact(@PathParam("id") int id)
     {
-        return Response.ok("{contact_id: " + id + ", name: \"Dummy Name\", phone: \"+0123456789\" }").build();
+        return Response.ok(new Contact(id, "John", "Doe", "+123456789")).build();
     }
 
     @POST
-    public Response createContact(@FormParam("name") String name, @FormParam("phone") String phone)
+    public Response createContact(Contact contact)
     {
         return Response.created(null).build();
     }
@@ -30,9 +32,9 @@ public class ContactResource {
 
     @PUT
     @Path("/{id}")
-    public Response updateContact(@PathParam("id") int id, @FormParam("name") String name,
-                                  @PathParam("phone") String phone)
+    public Response updateContact(@PathParam("id") int id,
+                                  Contact contact)
     {
-        return Response.ok("{contact_id: " + id + ", name: \"" + name + "\", phone: \"" + phone + "\" }").build();
+        return Response.ok(new Contact(id, contact.getFirstName(), contact.getLastName(), contact.getPhone())).build();
     }
 }
